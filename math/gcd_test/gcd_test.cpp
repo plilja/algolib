96e86912ae0f6db65a2d09d41984d21d6dfcfc40
patrik <patlil@kth.se>
2015-04-19 11:28:30 +0200

    gcd test

---
+++ math/gcd_test/gcd_test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "../gcd.h"
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int a, b;
+        cin >> a >> b;
+        int ans = gcd(a, b);
+        cout << ans << endl;
+    }
+
+}
