405e7f94267da0f7df12f7b8ab8a9854081b420e
patrik <patlil@kth.se>
2015-04-19 11:07:44 +0200

    Some cleanups of gaussjordan

--- math/equationsolver_test/equationsolver.cpp
+++ math/equationsolver_test/equationsolver.cpp
@@ -1,52 +1,41 @@
-#include "gaussjordan.h"
+#include "../gaussjordan.h"
 #include <vector>
-#include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main()
 {
-	int n;
-	while(true)
-	{
-		scanf("%d", &n);
-		if(n==0)
-			break;
-		vector<double> x;
+    int n;
+    while (true) {
+        scanf("%d", &n);
+        if (n == 0)
+            break;
+        vector<double> x;
         vector<vector<double> > A(n, vector<double>(n));
-        //fread(A, sizeof(double), n * n, stdin);
-		vector<double> y(n);
+        vector<double> y(n);
         double tmp;
-		for(int i=0; i<n; ++i)
-		{
-			for(int j=0; j<n; ++j)
-			{
-				scanf("%lf", &tmp);
-				A[i][j]=tmp;
-			}
-		}
-		for(int i=0; i<n; ++i)
-		{
-			scanf("%lf", &tmp);
-			y[i]= tmp;
-		}
-		int ans=gaussjordan<double>(A, y, x);
-		if(ans==INCONSISTENT)
-		{
-			printf("inconsistent\n");
-		}
-		else if(ans==MULTIPLE)
-		{
-			printf("multiple\n");
-		}
-		else if(ans==SUCCESS)
-		{
-			for(int i=0; i<n; ++i)
-			{
-				cout<<x[i]<<" ";
-			}
-			cout<<endl;
-		}
-	}
-	return 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                scanf("%lf", &tmp);
+                A[i][j] = tmp;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            scanf("%lf", &tmp);
+            y[i] = tmp;
+        }
+        auto ans = gaussjordan<double>(A, y, x);
+        if (ans == INCONSISTENT) {
+            printf("inconsistent\n");
+        } else if (ans == MULTIPLE) {
+            printf("multiple\n");
+        } else if (ans == SUCCESS) {
+            for (int i = 0; i < n; ++i) {
+                printf("%.4lf ", x[i]);
+            }
+            puts("");
+        }
+    }
+    return 0;
 }
--- math/equationsolverplus_test/equationsolverplus.cpp
+++ math/equationsolverplus_test/equationsolverplus.cpp
@@ -1,4 +1,4 @@
-#include "gaussjordan.h"
+#include "../gaussjordan.h"
 #include <vector>
 #include <iostream>
 
@@ -6,49 +6,43 @@ using namespace std;
 
 int main()
 {
-	int n;
-	while(true)
-	{
-		scanf("%d", &n);
-		if(n==0)
-			break;
-		vector<pair<bool, double> > x;
+    int n;
+    while (true) {
+        scanf("%d", &n);
+        if (n == 0)
+            break;
+        vector<pair<bool, double> > x;
         vector<vector<double> > A(n, vector<double>(n));
-		vector<double> y(n);
+        vector<double> y(n);
         double tmp;
-		for(int i=0; i<n; ++i)
-		{
-			for(int j=0; j<n; ++j)
-			{
-				scanf("%lf", &tmp);
-				A[i][j]=tmp;
-			}
-		}
-		for(int i=0; i<n; ++i)
-		{
-			scanf("%lf", &tmp);
-			y[i]=tmp;
-		}
-		int ans=gaussjordanplus<double>(A, y, x);
-		if(ans==INCONSISTENT)
-		{
-			printf("inconsistent\n");
-		}
-		else if(ans==MULTIPLE || ans==SUCCESS)
-		{
-			for(int i=0; i<n; ++i)
-			{
-				if(x[i].first)
-					if(isZero(x[i].second))
-						cout<<"0 ";
-					else
-						cout<<x[i].second<<" ";
-				else
-					cout<<"? ";
-			}
-			cout<<endl;
-		}
-		
-	}
-	return 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                scanf("%lf", &tmp);
+                A[i][j] = tmp;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            scanf("%lf", &tmp);
+            y[i] = tmp;
+        }
+        auto ans = gaussjordanplus<double>(A, y, x);
+        if (ans == INCONSISTENT) {
+            printf("inconsistent\n");
+        } else if (ans == MULTIPLE || ans == SUCCESS) {
+            for (int i = 0; i < n; ++i) {
+                if (x[i].first) {
+                    if (is_zero(x[i].second)) {
+                        printf("0.0000");
+                    } else {
+                        printf("%.4lf ", x[i].second);
+                    }
+                } else {
+                    printf("? ");
+                }
+            }
+            puts("");
+        }
+
+    }
+    return 0;
 }
