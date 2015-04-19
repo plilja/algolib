d3e0aded59e24babb7e0cf48355f17cc630cb018
patrik <patlil@kth.se>
2015-04-19 15:07:57 +0200

    retab

--- math/primesieve.h
+++ math/primesieve.h
@@ -5,35 +5,35 @@
 typedef unsigned int uint;
 
 /*
-	Provides a data structure holding information about
-	which numbers are prime and which aren't below a given 
-	limit. 
+    Provides a data structure holding information about
+    which numbers are prime and which aren't below a given 
+    limit. 
 */
 class PrimeSieve
 {
-	private:
+    private:
         std::vector<bool> primes;
-		int nr_of_primes;
-		uint sieve_size;
-	public:
-		/*
-		 * Construct a prime sieve of size n using
-		 * the sieve of eratosthenes algorithm. 
-		 * 
-		 * Parameters:
-		 * n : The size of the sieve (inclusive)
-		 */
-		PrimeSieve(uint n);
-		
-		/*
-		 * The number of primes in the sieve. 
-		 */
-		int num_primes();
-		
-		/*
-		 * Tells if n is a prime number.
-		 * 
-		 * Running time O(1) 
-		 */
-		bool is_prime(uint n) const;
+        int nr_of_primes;
+        uint sieve_size;
+    public:
+        /*
+         * Construct a prime sieve of size n using
+         * the sieve of eratosthenes algorithm. 
+         * 
+         * Parameters:
+         * n : The size of the sieve (inclusive)
+         */
+        PrimeSieve(uint n);
+        
+        /*
+         * The number of primes in the sieve. 
+         */
+        int num_primes();
+        
+        /*
+         * Tells if n is a prime number.
+         * 
+         * Running time O(1) 
+         */
+        bool is_prime(uint n) const;
 };
