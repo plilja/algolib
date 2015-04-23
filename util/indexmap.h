7578684332846e9a1bb97fb3493192215b04a030
patrik <patlil@kth.se>
2015-04-24 00:24:53 +0200

    Reformat

--- util/indexmap.h
+++ util/indexmap.h
@@ -12,28 +12,30 @@
 #include <string>
 #include <unordered_map>
 
-template <typename T>
+template<typename T>
 class indexmap
 {
 private:
-    std::vector<T> ind; //Maps int->T
+    std::vector <T> ind; //Maps int->T
     std::unordered_map<T, int> objects; //Maps T->int
 public:
 
-	indexmap() {
+    indexmap()
+    {
     }
 
-	/*
-	 * Insert an object in the map. Returns the index ascociated with
-	 * the object. The index returned will start at 0 for the first
-	 * object inserted and increase by 1 for each object inserted. 
-	 * 
-	 * If the object already is in the map, the index it was given when it was
-	 * first inserted will be returned. 
-	 * 
-	 * Runs in time O(1)
-	 */
-	int get_index(T o) {
+    /*
+     * Insert an object in the map. Returns the index ascociated with
+     * the object. The index returned will start at 0 for the first
+     * object inserted and increase by 1 for each object inserted.
+     *
+     * If the object already is in the map, the index it was given when it was
+     * first inserted will be returned.
+     *
+     * Runs in time O(1)
+     */
+    int get_index(T o)
+    {
         std::pair<T, int> p(o, ind.size());
         auto r = objects.insert(p);
         if (r.second) {
@@ -41,23 +43,25 @@ public:
         }
         return r.first->second;
     }
-	
-	/*
-	 * Get the object ascociated with the integer i. The integer must be 
-	 * already be ascociated with an object in the map. 
-	 * 
-	 * Runs in time O(1)
-	 */
-    T get_object(int i) {
+
+    /*
+     * Get the object ascociated with the integer i. The integer must be
+     * already be ascociated with an object in the map.
+     *
+     * Runs in time O(1)
+     */
+    T get_object(int i)
+    {
         return ind[i];
     }
-	
-	/*
-	 * Get the current size of the indexmap. 
-	 * 
-	 * Runs in time O(1)
-	 */
-	int size() {
+
+    /*
+     * Get the current size of the indexmap.
+     *
+     * Runs in time O(1)
+     */
+    int size()
+    {
         return ind.size();
     }
 };
