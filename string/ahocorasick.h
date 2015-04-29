e7b7be880517d47746c7b968ee7b36a6f9b28b8a
patrik <patlil@kth.se>
2015-04-29 18:40:32 +0200

    Limitation isn't true any more

--- string/ahocorasick.h
+++ string/ahocorasick.h
@@ -7,9 +7,6 @@
 /*
  * An implementations of the Aho-Corasick pattern matching algorithm. 
  * 
- * The total number of characters in all of the patterns that are being 
- * searched may not exceed 100000. 
- * 
  * Time-complexity is O(text.length+sum(patterns.length)+#hits)
  * 
  * Parameters:
@@ -20,11 +17,6 @@
  * Vector of vectors where an inner vector v_i contains the indexes
  * where the i:th patterns has been found in the text. Indexes start
  * at 0 for the first position in the text.
- * 
- * Se article below for a description of the algorithm. 
- * Aho, Alfred V.; Margaret J. Corasick (June 1975). 
- * "Efficient string matching: An aid to bibliographic search"
- * 
  */
 std::vector<std::vector<int> > ahocorasick(const std::string &text, const std::vector<std::string> &patterns);
 
